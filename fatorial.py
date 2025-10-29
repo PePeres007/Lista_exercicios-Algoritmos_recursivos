e523bfb4250e62ed57b452d7d921a1873b9bab38
Pedro Peres <166012767+PePeres007@users.noreply.github.com>
2025-10-29 16:30:38 -0300

    Add input function for integer validation

--- fatorial.py
+++ fatorial.py
@@ -1,3 +1,12 @@
+def ler_inteiro():
+    while True:
+        entrada = input("Digite um número inteiro: ")
+        try:
+            n = int(entrada)
+            return n
+        except ValueError:
+            print("Valor inválido! Por favor, digite apenas um número inteiro.")
+
 def fatorial(n):
     if n < 0:
         return 0
@@ -6,5 +15,5 @@ def fatorial(n):
     else:
         return n * fatorial(n - 1)
 
-n = int(input("Digite o número que deseja ver o fatorial: "))
+n = ler_inteiro()
 print(fatorial(n))
